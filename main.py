005e4b0c96ab45d12bbc0846aace5345a4746a4f
Shadow-King04 <godismyjudgemaynard@gmail.com>
2025-10-02 18:02:08 +0100

    draws player to screen, add player rotation left and right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,13 +10,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameclock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        
+                
         dt = gameclock.tick(60) / 1000
 
 if __name__ == "__main__":
